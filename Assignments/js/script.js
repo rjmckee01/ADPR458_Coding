fb0139039bfc37d764320ba2bc4d57ea128edf1b
Rex McKeeman <rexmckeeman9@gmail.com>
2025-03-27 12:32:14 -0500

    3/27 class

--- Assignments/js/script.js
+++ Assignments/js/script.js
@@ -1,7 +1,7 @@
 const toggleBtn = document.getElementById("toggle-button");
 const navListItems = document.getElementById("nav-menu-list");
-console.log("Howdy!");
 
-toggleBtn.addEventListener('click', () => {
+
+toggleBtn.addEventListener('click', function () {
     navListItems.classList.toggle('active');
-})
+});
